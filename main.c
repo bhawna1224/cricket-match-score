67342a2d0b7189419f3e8d2f4f4efdd7846c3698
bhawna1224 <132734024+bhawna1224@users.noreply.github.com>
2024-03-19 23:37:15 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,273 +1,159 @@
-// C program for the above approach
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <windows.h>
 
-// Structure of the employee
-struct emp {
-	char name[50];
-	float salary;
-	int age;
-	int id;
+struct batsman
+{
+    char name[50] ;
+    int runs , score , balls ,ones , twos , threes , fours , sixes ;
 };
-struct emp e;
-
-// size of the structure
-long int size = sizeof(e);
-
-// In the start coordinates
-// will be 0, 0
-COORD cord = { 0, 0 };
-
-// function to set the
-// coordinates
-void gotoxy(int x, int y)
+struct batsman bat;
+struct bowler
 {
-	cord.X = x;
-	cord.Y = y;
-	SetConsoleCursorPosition(
-		GetStdHandle(STD_OUTPUT_HANDLE),
-		cord);
+    char name[50];
+    int runs_given, wickets, overs;
+};
+struct bowler bowl;
+
+long int size = sizeof(bat);
+FILE *fp,*ft;
+
+void addrecord_batsman()
+{
+    system("cls");
+    fseek(fp, 0, SEEK_END);
+    char another = 'y';
+    int choice;
+    while (another == 'y') {
+        printf("Enter the Batsman details  :\n");
+
+        printf("Enter name of batsman:\n");
+        scanf("%s",&bat.name);
+        printf("Enter the number of ones scored by player:\n ");
+        scanf("%d",&bat.ones);
+        printf("Enter the number of twos scored by player:\n ");
+        scanf("%d",&bat.twos);
+        printf("Enter the number of threes scored by player:\n ");
+        scanf("%d",&bat.threes);
+        printf("Enter the number of fours scored by player:\n ");
+        scanf("%d",&bat.fours);
+        printf("Enter the number of sixes scored by player:\n ");
+        scanf("%d",&bat.sixes);
+        printf("Enter the balls played by the player:\n");
+        scanf("%d",&bat.balls);
+        fwrite(&bat, size, 1, fp);
+        printf("Enter the Bowlers details  :\n");
+
+        printf("\nWant to add another record (Y/N) : ");
+        scanf("%c", &another);
+    }
 }
 
-FILE *fp, *ft;
 
-// Function to add the records
-void addrecord()
+void addrecord_bowler()
 {
-	system("cls");
-	fseek(fp, 0, SEEK_END);
-	char another = 'y';
+    system("cls");
+    fseek(ft, 0, SEEK_END);
+    char another = 'y';
+    int choice;
 
-	while (another == 'y') {
-		printf("\nEnter Name : ");
-		scanf("%s", e.name);
+    while (another == 'y') {
 
-		printf("\nEnter Age : ");
-		scanf("%d", &e.age);
-
-		printf("\nEnter Salary : ");
-		scanf("%f", &e.salary);
-
-		printf("\nEnter EMP-ID : ");
-		scanf("%d", &e.id);
-
-		fwrite(&e, size, 1, fp);
-
-
-		fflush(stdin);
-
-
-	}
+        printf("Enter name of bowler:");
+        scanf("%s", &bowl.name);
+        printf("Enter the runs given by the bowler:\n ");
+        scanf("%d", &bowl.runs_given);
+        printf("Enter the overs bowled by the bowler:\n");
+        scanf("%d", &bowl.overs);
+        printf("Enter the wickets taken by the bowler\n");
+        scanf("%d", &bowl.wickets);
+        fwrite(&bowl, size, 1, ft);
+        printf("\nWant to add another record (Y/N) : ");
+        scanf("%c", &another);
+    }
 }
 
-// Function to delete the records
-void deleterecord()
+void displayrecord_batsman()
 {
-	system("cls");
-	char empname[50];
-	char another = 'y';
+    system("cls");
 
-	while (another == 'y') {
-		printf("\nEnter employee "
-			"name to delete : ");
-		scanf("%s", empname);
+    rewind(fp);
+    printf("=====================================================================================================");
+    printf("\nNAME\t\tONES\t\tTWOS\t\tTHREES\t\tFOURS\t\tSIXES\t\tBALLS\n");
+    printf("=====================================================================================================\n");
 
-		ft = fopen("temp.txt", "wb");
-		rewind(fp);
+    while (fread(&bat, size, 1, fp) == 1)
+        printf("\n%s\t%d\t%d\t%d\t%d\t%d\t%d",bat.name, bat.ones, bat.twos, bat.threes,bat.fours,bat.sixes,bat.balls);
 
-		while (fread(&e, size,
-					1, fp)
-			== 1) {
-			if (strcmp(e.name,
-					empname)
-				!= 0)
-				fwrite(&e, size, 1, ft);
-		}
-
-		fclose(fp);
-		fclose(ft);
-		remove("data.txt");
-		rename("temp.txt", "data.txt");
-		fp = fopen("data.txt", "rb+");
-
-		printf("\nWant to delete another"
-			" record (Y/N) :");
-		fflush(stdin);
-		another = getche();
-	}
+    printf("\n\n\n\t");
+    system("pause");
 }
 
-// Function to display the record
-void displayrecord()
+void displayrecord_bowler()
 {
-	system("cls");
+    system("cls");
 
-	// sets pointer to start
-	// of the file
-	rewind(fp);
+    rewind(ft);
+    printf("\n==========================================================");
+    printf("\nNAME\t\tOVERS\t\tWICKETS\t\tRUNS\n");
+    printf("==========================================================\n");
 
-	printf("\n========================="
-		"==========================="
-		"======");
-	printf("\nNAME\t\tAGE\t\tSALARY\t\t"
-		"\tID\n",
-		e.name, e.age,
-		e.salary, e.id);
-	printf("==========================="
-		"==========================="
-		"====\n");
+    while (fread(&bowl, size, 1, ft) == 1)
+        printf("\n%s\t%d\t%d\t%d",bowl.name,bowl.overs,bowl.runs_given,bowl.wickets);
 
-	while (fread(&e, size, 1, fp) == 1)
-		printf("\n%s\t\t%d\t\t%.2f\t%10d",
-			e.name, e.age, e.salary, e.id);
-
-	printf("\n\n\n\t");
-	system("pause");
+    printf("\n\n\n\t");
+    system("pause");
 }
 
-// Function to modify the record
-void modifyrecord()
-{
-	system("cls");
-	char empname[50];
-	char another = 'y';
-
-	while (another == 'y') {
-		printf("\nEnter employee name"
-			" to modify : ");
-		scanf("%s", empname);
-
-		rewind(fp);
-
-		// While File is open
-		while (fread(&e, size, 1, fp) == 1) {
-			// Compare the employee name
-			// with ename
-			if (strcmp(e.name, empname) == 0) {
-				printf("\nEnter new name:");
-				scanf("%s", e.name);
-				printf("\nEnter new age :");
-				scanf("%d", &e.age);
-				printf("\nEnter new salary :");
-				scanf("%f", &e.salary);
-				printf("\nEnter new EMP-ID :");
-				scanf("%d", &e.id);
-
-				fseek(fp, -size, SEEK_CUR);
-				fwrite(&e, size, 1, fp);
-				break;
-			}
-		}
-
-		// Ask for modifying another record
-		printf("\nWant to modify another"
-			" record (Y/N) :");
-		fflush(stdin);
-		scanf("%c", &another);
-	}
-}
-
-// Driver code
 int main()
 {
-	int choice;
+    int choice;
 
-	// opening the file
-	fp = fopen("data.txt", "rb+");
+    fp = fopen("data1.txt", "r+");
+    if (fp == NULL) {
+        fp = fopen("data1.txt", "w+");
+        if (fp == NULL) {
+            printf("\nCannot open file...");
+            exit(1);
+        }
+    }
+    ft = fopen("data2.txt", "r+");
+    if (ft == NULL) {
+        ft = fopen("data2.txt", "w+");
+        if (ft == NULL) {
+            printf("\nCannot open file...");
+            exit(1);
+        }
+    }
 
-	// showing error if file is
-	// unable to open.
-	if (fp == NULL) {
-		fp = fopen("data.txt", "wb+");
-		if (fp == NULL) {
-			printf("\nCannot open file...");
-			exit(1);
-		}
-	}
-
-	system("Color 3F");
-	printf("\n\n\n\n\t\t\t\t============="
-		"============================="
-		"===========");
-	printf("\n\t\t\t\t~~~~~~~~~~~~~~~~~~~"
-		"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-		"~~~~~");
-	printf("\n\t\t\t\t==================="
-		"============================="
-		"=====");
-	printf("\n\t\t\t\t[|:::>:::>:::>::> "
-		"EMPLOYEE RECORD <::<:::<:::"
-		"<:::|]\t");
-	printf("\n\t\t\t\t==================="
-		"============================="
-		"=====");
-	printf("\n\t\t\t\t~~~~~~~~~~~~~~~~~~~~"
-		"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-		"~~~");
-	printf("\n\t\t\t\t====================="
-		"==============================\n");
-	printf("\n\n\n\t\t\t\t\t\t\t\t\t\t"
-		"Developer : @Sushant_Gaurav"
-		"\n\n\t\t\t\t");
-
-	system("pause");
-
-	while (1) {
-		// Clearing console and asking the
-		// user for input
-		system("cls");
-		gotoxy(30, 10);
-		printf("\n1. ADD RECORD\n");
-		gotoxy(30, 12);
-		printf("\n2. DELETE RECORD\n");
-		gotoxy(30, 14);
-		printf("\n3. DISPLAY RECORDS\n");
-		gotoxy(30, 16);
-		printf("\n4. MODIFY RECORD\n");
-		gotoxy(30, 18);
-		printf("\n5. EXIT\n");
-		gotoxy(30, 20);
-		printf("\nENTER YOUR CHOICE...\n");
-		fflush(stdin);
-		scanf("%d", &choice);
-
-		// Switch Case
-		switch (choice) {
-		case 1:
-
-			// Add the records
-			addrecord();
-			break;
-
-		case 2:
-
-			// Delete the records
-			deleterecord();
-			break;
-
-		case 3:
-
-			// Display the records
-			displayrecord();
-			break;
-
-		case 4:
-
-			// Modify the records
-			modifyrecord();
-			break;
-
-		case 5:
-			fclose(fp);
-			exit(0);
-			break;
-
-		default:
-			printf("\nINVALID CHOICE...\n");
-		}
-	}
-
-	return 0;
+    while (1) {
+        printf("\n 1. ADD RECORD FOR BATSMAN\n");
+        printf("\n 2. ADD RECORD FOR BOWLER\n");
+        printf("\n 3. DISPLAY RECORDS FOR BATSMAN\n");
+        printf("\n 4. DISPLAY RECORDS FOR BOWLER\n");
+        printf("\n 5. EXIT\n");
+        printf("\nENTER YOUR CHOICE...\n");
+        scanf("%d", &choice);
+        switch (choice) {
+        case 1:
+            addrecord_batsman();
+            break;
+        case 2:
+            addrecord_bowler();
+            break;
+        case 3:
+            displayrecord_batsman();
+            break;
+        case 4:
+            displayrecord_bowler();
+            break;
+        case 5:
+            fclose(fp);
+            fclose(ft);
+            exit(0);
+            break;
+        default:
+            printf("INVALID CHOICE...\n");
+        }
+    }
+    return 0;
 }
